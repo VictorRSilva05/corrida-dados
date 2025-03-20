bef1e19df037a7e9edddaa32f36ad70304d8d613
Victor R. Silva <victorr.silva005@gmail.com>
2025-03-20 08:28:46 -0300

    Implementada lógica de lançamento de dado

--- CorridaDados/Program.cs
+++ CorridaDados/Program.cs
@@ -1,75 +1,33 @@
-﻿using CorridaDados.Entities;
+﻿
+namespace CorridaDados;
 
-namespace CorridaDados
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        while (true)
         {
-            Tabuleiro tabuleiro = new Tabuleiro();
-
-            Console.WriteLine("##### C O R R I D A #####");
-            Console.Write("\nInsira seu nome: ");
-            string nome = Console.ReadLine();
-
-            while(String.IsNullOrEmpty(nome))
-            {
-                Console.Write("Por favor insira seu nome: ");
-                nome = Console.ReadLine();
-            }
-
-            Jogador jogador1 = new Jogador(nome);
-            Jogador jogador2 = new Jogador(System.Environment.MachineName);
-
             Console.Clear();
-            Console.WriteLine($"{jogador1.Nome} VS {jogador2.Nome}");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Jogo dos Dados");
+            Console.WriteLine("------------------------------------");
 
-            int turno = default;
-            int valor = default;
+            Console.Write("Pressione ENTER para lançar o dado...");
+            Console.ReadLine();
 
-            Console.WriteLine("\n");
+            Random geradorDeNumeros = new Random();
 
-            while (jogador2.Ganhador == false || jogador1.Ganhador == false)
-            {
-                turno++;
+            int resultado = geradorDeNumeros.Next(1, 7);
 
-                if(turno % 2 == 0)
-                {
-                    valor = jogador1.RolaDado();
-                    Console.WriteLine($"\n{jogador1.Nome} tirou {valor}");
-                    tabuleiro.Avancar(valor, jogador1);
-                    Console.WriteLine(jogador1.Posicao);
-                    jogador1.Vitoria();
-                    tabuleiro.ImprimirTabuleiro(jogador1.Posicao);
-                    Console.WriteLine("\nAperte para continuar");
-                    Console.ReadKey();
-                    Console.Clear();
-                }
-                else
-                {
-                    valor = jogador2.RolaDado();
-                    Console.WriteLine($"\n{jogador2.Nome} tirou {valor}");
-                    tabuleiro.Avancar(valor, jogador2);
-                    Console.WriteLine(jogador2.Posicao);
-                    jogador2.Vitoria();
-                    tabuleiro.ImprimirTabuleiro(jogador2.Posicao);
-                }
-               
-                if(jogador1.Ganhador == true)
-                {
-                    Console.Clear();
-                    Console.WriteLine($"{jogador1.Nome} é o vencedor!!!!");
-                    goto Fim;
-                }
-                else if(jogador2.Ganhador == true)
-                {
-                    Console.Clear();
-                    Console.WriteLine($"{jogador2.Nome} é o vencedor!!!!");
-                    goto Fim;
-                }
-            }
-        Fim:
-            Console.WriteLine("Aperte qualquer tecla para sair");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine($"O dado caiu no número: {resultado}");
+            Console.WriteLine("------------------------------------");
+
+            Console.Write("Deseja continuar? (s/n): ");
+            string opcaoContinuar = Console.ReadLine().ToUpper();
+
+            if (opcaoContinuar != "S")
+                break;
         }
     }
 }
